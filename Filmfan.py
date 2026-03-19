4883146c352045c91e961ba448a47af38497cd6a
3eNVBesteNV <jessebeintema56@gmail.com>
2026-03-19 18:16:14 +0100

    registreren naar dbs en flash message voor inlog met incorrecte dbs informatie

--- Filmfan.py
+++ Filmfan.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template,redirect
+import sqlite3
+def get_db():
+    conn=sqlite3.connect("filmfan.db")
+    conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys=ON;")
+    return conn
+
+from flask import Flask, render_template,redirect,flash
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired,EqualTo
 from wtforms import EmailField, PasswordField, StringField, SubmitField, DateField
@@ -29,13 +36,42 @@ class RegistrationForm(FlaskForm):
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        pass
+        email = form.email.data
+        wachtwoord = form.wachtwoord.data
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM user WHERE email = ? AND password = ?",(email,wachtwoord))
+        row = cursor.fetchone()
+        db.close()
+        if row is None:
+            flash ('wachtwoord en email komen niet overeen','danger')
+        else:
+            flash ("login succesvol",'success')
+        return redirect("/login")
     return render_template("login.html",form=form)
 
 @app.route("/register", methods = ["GET","POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.wachtwoord.data
+        geboortedatum = form.geboortedatum.data.strftime('%Y-%m-%d')  # Sla datum op als YYYY-MM-DD
+
+        db = get_db()
+        try:
+            db.execute(
+                "INSERT INTO user (username, email, password, geboortedatum) VALUES (?, ?, ?, ?)",
+                (username, email, password, geboortedatum)
+            )
+            db.commit()
+        except sqlite3.IntegrityError:
+            # Als username of email al bestaat
+            return "Gebruikersnaam of email bestaat al!"
+        finally:
+            db.close()
+
         return redirect("/")
     return render_template("registreren.html",form=form)
         

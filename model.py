6ac3166068e0bc2f66852b502387b596c256739f
WijnetikettenPassie <TBugginkbusiness@outlook.com>
2026-03-25 09:32:14 +0100

    Classes aangepast

--- model.py
+++ model.py
@@ -1,42 +1,42 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, Text, Date, ForeignKey
 db = SQLAlchemy()
 
 
-
 class Acteur(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    voornaam: Mapped[str] = mapped_column(Text)
+    achternaam: Mapped[str] = mapped_column(Text)
 
-    id = db.Column(db.Integer,primary_key=True)
-    voornaam = db.Column(db.Text)
-    achternaam = db.Column(db.Text)
-
-    def __init__(self, voornaam,achternaam):
+    def __init__(self, voornaam: str, achternaam: str):
         self.voornaam = voornaam
         self.achternaam = achternaam
+
 
 class Regisseur(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    voornaam = db.Column(db.Text)
-    achternaam = db.Column(db.Text)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    voornaam: Mapped[str] = mapped_column(Text)
+    achternaam: Mapped[str] = mapped_column(Text)
 
-    def __init__(self, voornaam,achternaam):
+    def __init__(self, voornaam: str, achternaam: str):
         self.voornaam = voornaam
         self.achternaam = achternaam
 
+
 class Rol(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    acteur_id = db.Column(db.Integer, db.ForeignKey('acteur.id'))
-    film_id = db.Column(db.Integer,db.ForeignKey('film.id'))
-    personage = db.Column(db.Text)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    acteur_id: Mapped[int] = mapped_column(ForeignKey("acteur.id"))
+    film_id: Mapped[int] = mapped_column(ForeignKey("film.id"))
+    personage: Mapped[str] = mapped_column(Text)
+
 
 class Film(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.Text)
-    regisseur_id = db.Column(db.Integer,db.ForeignKey('regisseur.id'))
-    release_datum = db.Column(db.Date)
-    trailer_url = db.Column(db.Text)
-
-
-
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(Text)
+    regisseur_id: Mapped[int] = mapped_column(ForeignKey("regisseur.id"))
+    release_datum: Mapped[Date] = mapped_column(Date)
+    trailer_url: Mapped[str] = mapped_column(Text)
+    
